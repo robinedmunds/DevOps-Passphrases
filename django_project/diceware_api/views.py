452bf7258a0d93e6695e3d596c9810a7f1ed7d53
Robin Edmunds <r.edmunds@live.co.uk>
2020-08-28 20:51:04 +0100

    Add diceware/ route and view. Successfully responding

--- django_project/diceware_api/views.py
+++ django_project/diceware_api/views.py
@@ -2,6 +2,12 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from diceware_api.serializers import UserSerializer, GroupSerializer
+# normal views
+from rest_framework import authentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .business.diceware.classes.phrases import Phrases
+from .business.diceware.parse_wordlist import from_file as parse_wordlist_from_file
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +26,20 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class BasicDicewareView(APIView):
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+        wordlist = parse_wordlist_from_file(
+            "diceware_api/business/wordlists/eff_short_wordlist_1.txt")
+        kwargs = {
+            "wordlist": wordlist,
+            "word_count": 6,
+            "separator": " ",
+            "phrase_count": 10
+        }
+        phrases = Phrases(**kwargs).as_dict()
+        return Response(phrases)
