a178614658c2601c799111565bb279355213994e
Robin Edmunds <r.edmunds@live.co.uk>
2020-09-02 20:43:38 +0100

    Add wordlist selection to API view

--- django_project/diceware_api/views.py
+++ django_project/diceware_api/views.py
@@ -8,6 +8,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .business.diceware.classes.phrases import Phrases
 from .business.diceware.parse_wordlist import from_file as parse_wordlist_from_file
+from .helpers import get_wordlists
+
+WORDLIST_DIR = "diceware_api/business/wordlists/"
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -32,37 +35,41 @@ class DicewarePassphrasesView(APIView):
     # authentication_classes = [authentication.TokenAuthentication]
     # permission_classes = [permissions.IsAdminUser]
     def get(self, request, format=None):
-        wordlist = parse_wordlist_from_file(
-            "diceware_api/business/wordlists/eff_short_wordlist_1.txt")
         kwargs = {
-            "wordlist": wordlist,
+            "wordlist": "eff_short_wordlist_1.txt",
             "word_count": 6,
             "separator": " ",
             "phrase_count": 1
         }
-        query_params = request.query_params
+        wordlist_name = kwargs["wordlist"]
+        wordlist_files = get_wordlists(WORDLIST_DIR)
+        q = request.query_params
         try:
-            if "word_count" in query_params:
-                value = int(query_params["word_count"])
-                if value < 1:
-                    value = abs(value)
+            if "word_count" in q:
+                value = abs(int(q["word_count"]))
                 if value > 30:
                     value = 30
                 kwargs["word_count"] = value
-            if "phrase_count" in query_params:
-                value = int(query_params["phrase_count"])
-                if value < 1:
-                    value = abs(value)
+            if "phrase_count" in q:
+                value = abs(int(q["phrase_count"]))
                 if value > 50:
                     value = 50
                 kwargs["phrase_count"] = value
-            if "separator" in query_params:
-                kwargs["separator"] = query_params["separator"][0]
+            if "separator" in q:
+                kwargs["separator"] = q["separator"][0]
+            if "wordlist" in q:
+                if q["wordlist"] not in wordlist_files:
+                    raise Exception
+                wordlist_name = q["wordlist"]
         except Exception:
-            return Response("Bad request. Check your query paramaters. Valid query paramaters are: phrase_count=(integer), word_count=(integer), separator=(character).", status=400)
+            return Response("Bad request. Check your query paramaters. Valid query paramaters are: wordlist=(wordlist name), phrase_count=(integer), word_count=(integer), separator=(character).", status=400)
 
+        kwargs["wordlist"] = parse_wordlist_from_file(
+            WORDLIST_DIR + wordlist_name)
         phrases = Phrases(**kwargs).as_dict()
+        phrases["wordlist"] = wordlist_name
         return Response({
             "query": request.query_params,
+            "wordlists_available": wordlist_files,
             "phrases": phrases
         })
