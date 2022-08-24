2ab8ee38131b7cc943df5f058dc3e9328038d9b0
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-08-24 16:38:44 +0100

    Move viewport meta line from _document to _app

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 import CssBaseline from '@mui/material/CssBaseline'
 import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <CssBaseline />
       <Layout>
         <Component {...pageProps} />
