ccab6c0eb5e332b3e176ef98b87bb1aaddddb6a8
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-08-25 16:35:46 +0100

    Build phrase cards

--- nextjs/components/phrase.tsx
+++ nextjs/components/phrase.tsx
@@ -1,20 +1,42 @@
 import { Box } from '@mui/material'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 
-const Phrases = ({ phrase }) => {
+const Phrase = ({ phrase }) => {
   return (
     <Box
       sx={{
-        margin: "2rem",
-        width: 640,
-        height: 120,
-        backgroundColor: 'primary.dark',
-        '&:hover': {
-          backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
-        },
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        margin: "2.0rem 0 2.0rem",
+        backgroundColor: "#eee",
+        // columnGap: "4.0rem"
       }}
-    >{phrase}</Box>
+    >
+      <Box
+        sx={{
+          padding: "0 2.2rem"
+        }}
+      >
+        21 chars
+      </Box>
+      <Box
+        sx={{
+          fontSize: "2.0rem",
+          padding: "1.6rem 0"
+        }}
+      >
+        { phrase }
+      </Box>
+      <Box
+        sx={{
+          padding: "0 2.2rem"
+        }}
+      >
+        <ContentCopyIcon />
+      </Box>
+    </Box>
   )
 }
 
-export default Phrases
+export default Phrase
