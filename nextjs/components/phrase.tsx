aca36bad2b3c57845dcfac7c2dd837ae1b4d3b19
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-02 15:40:14 +0100

    Tweak styles of phrase card

--- nextjs/components/phrase.tsx
+++ nextjs/components/phrase.tsx
@@ -4,44 +4,50 @@ import { CopyToClipboard } from "react-copy-to-clipboard"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
 
 const Phrase = (props: { phrase: string }) => {
-  const [ copied, setIsCopied ] = useState(false)
+  const [copied, setIsCopied] = useState(false)
 
   return (
     <Box
       sx={{
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-evenly",
+        justifyContent: "space-between",
         margin: "2.0rem 0 2.0rem",
         backgroundColor: "#eee",
-      }}
-    >
+        textAlign: "center",
+        borderRadius: "10px",
+        "&:hover": {
+          backgroundColor: "#ddd"
+        }
+      }}>
       <Box
         sx={{
           padding: "0 2.2rem"
-        }}
-      >
+        }}>
         {props.phrase.length} chars
       </Box>
       <Box
         sx={{
           fontSize: "2.0rem",
           padding: "1.6rem 0"
-        }}
-      >
-        { props.phrase }
+        }}>
+        {props.phrase}
       </Box>
       <Box
         sx={{
           padding: "0 2.2rem"
-        }}
-      >
-        <CopyToClipboard text={props.phrase}
+        }}>
+        <CopyToClipboard
+          sx={{
+            cursor: "pointer",
+            "&:hover": {
+              color: "red"
+            }
+          }}
+          text={props.phrase}
           onCopy={() => {
             setIsCopied(true)
-            console.log("phrase copied")
-          }}
-          >
+          }}>
           <ContentCopyIcon />
         </CopyToClipboard>
       </Box>
@@ -50,3 +56,4 @@ const Phrase = (props: { phrase: string }) => {
 }
 
 export default Phrase
+
