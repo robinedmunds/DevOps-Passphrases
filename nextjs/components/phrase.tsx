2d7a88a1d0f209327aeb3fcaabb63602f97f689b
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-04 02:15:55 +0100

    Add Snackbar alert on copy of passphrase

--- nextjs/components/phrase.tsx
+++ nextjs/components/phrase.tsx
@@ -2,56 +2,64 @@ import { useState } from "react"
 import { Box } from "@mui/material"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
+import Snackbar from "./snackbar"
 
 const Phrase = (props: { phrase: string }) => {
   const [copied, setIsCopied] = useState(false)
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        margin: "2.0rem 0 2.0rem",
-        backgroundColor: "#eee",
-        textAlign: "center",
-        borderRadius: "10px",
-        "&:hover": {
-          backgroundColor: "#ddd"
-        }
-      }}>
+    <>
       <Box
         sx={{
-          padding: "0 2.2rem"
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          margin: "2.0rem 0 2.0rem",
+          backgroundColor: "#eee",
+          textAlign: "center",
+          borderRadius: "10px",
+          "&:hover": {
+            backgroundColor: "#ddd"
+          }
         }}>
-        {props.phrase.length} chars
-      </Box>
-      <Box
-        sx={{
-          fontSize: "2.0rem",
-          padding: "1.6rem 0"
-        }}>
-        {props.phrase}
-      </Box>
-      <Box
-        sx={{
-          padding: "0 2.2rem"
-        }}>
-        <CopyToClipboard
+        <Box
           sx={{
-            cursor: "pointer",
-            "&:hover": {
-              color: "red"
-            }
-          }}
-          text={props.phrase}
-          onCopy={() => {
-            setIsCopied(true)
+            padding: "0 2.2rem"
           }}>
-          <ContentCopyIcon />
-        </CopyToClipboard>
+          {props.phrase.length} chars
+        </Box>
+        <Box
+          sx={{
+            fontSize: "2.0rem",
+            padding: "1.6rem 0"
+          }}>
+          {props.phrase}
+        </Box>
+        <Box
+          sx={{
+            padding: "0 2.2rem"
+          }}>
+          <CopyToClipboard
+            sx={{
+              cursor: "pointer",
+              "&:hover": {
+                color: "red"
+              }
+            }}
+            text={props.phrase}
+            onCopy={() => {
+              setIsCopied(true)
+            }}>
+            <ContentCopyIcon />
+          </CopyToClipboard>
+        </Box>
       </Box>
-    </Box>
+      <Snackbar
+        message="Copied to clipboard"
+        isOpen={copied}
+        setIsOpen={setIsCopied}
+      />
+    </>
   )
 }
 
---
+++ nextjs/components/snackbar.tsx
@@ -0,0 +1,37 @@
+import { forwardRef } from "react"
+import Snackbar from "@mui/material/Snackbar"
+import MuiAlert, { AlertProps } from "@mui/material/Alert"
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
+
+const snackbar = (props: {
+  message: string
+  isOpen: boolean
+  setIsOpen: (flag: boolean) => {}
+}) => {
+  return (
+    <Snackbar
+      open={props.isOpen}
+      autoHideDuration={3000}
+      onClose={() => {
+        props.setIsOpen(false)
+      }}>
+      <Alert
+        onClose={() => {
+          props.setIsOpen(false)
+        }}
+        severity="info"
+        sx={{ width: "100%" }}>
+        {props.message}
+      </Alert>
+    </Snackbar>
+  )
+}
+
+export default snackbar
+
