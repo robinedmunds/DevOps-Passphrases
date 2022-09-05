8c4e61cd75f9df4911c4dd26a83d759eed7f682f
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-05 20:27:13 +0100

    Add new page, about

---
+++ nextjs/pages/about.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next"
+
+const about: NextPage = () => {
+  return (
+    <>
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A nam alias,
+        aut doloribus quibusdam explicabo quaerat error veniam quis aliquid vel
+        quo voluptas sapiente sunt, incidunt necessitatibus et quos eveniet.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A nam alias,
+        aut doloribus quibusdam explicabo quaerat error veniam quis aliquid vel
+        quo voluptas sapiente sunt, incidunt necessitatibus et quos eveniet.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A nam alias,
+        aut doloribus quibusdam explicabo quaerat error veniam quis aliquid vel
+        quo voluptas sapiente sunt, incidunt necessitatibus et quos eveniet.
+      </p>
+    </>
+  )
+}
+
+export default about
