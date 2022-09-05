d42f3be71ff8cf074b10c384b407895f8c40658c
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-05 20:14:43 +0100

    Make hero component text size responsive

--- nextjs/components/hero.tsx
+++ nextjs/components/hero.tsx
@@ -1,21 +1,22 @@
-import Box from "@mui/material/Box"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
 
 const Hero = () => {
+  const theme = createTheme()
+  theme.typography.h1 = {
+    fontSize: "3.0rem",
+    margin: "6.0rem 0",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "2.4rem"
+    }
+  }
+
   return (
-    <Box
-      sx={{
-        m: "6.0rem"
-      }}
-    >
-      <Typography
-        variant="h2"
-        align="center"
-        sx={{ backgroundColor: "lightgreen" }}
-      >
+    <ThemeProvider theme={theme}>
+      <Typography variant="h1" align="center">
         Generate strong, memorable and easy-to-type passphrases.
       </Typography>
-    </Box>
+    </ThemeProvider>
   )
 }
 
