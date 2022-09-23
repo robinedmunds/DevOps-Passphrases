77059449d9dad25f84ddc7a4119aaf919e72d1e0
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-23 23:02:28 +0100

    Refactor flask api view

--- backend/api.py
+++ backend/api.py
@@ -5,53 +5,69 @@ from business.diceware.parse_wordlist import from_file as parse_wordlist_from_fi
 from helpers import get_wordlists
 
 app = Flask(__name__)
-WORDLIST_DIR = "business/wordlists/"
+WORDLIST_FILES = get_wordlists("business/wordlists/")
+DEFAULT_OPTS = {
+    "wordlist": "eff_short_wordlist_1.txt",
+    "word_count": 6,
+    "separator": " ",
+    "phrase_count": 1
+}
+MAX_WORDS = 30
+MAX_PHRASES = 50
+
+
+def parse_query(query):
+    options = DEFAULT_OPTS
+    query_keys = query.keys()
+    try:
+        # parse
+        if "word_count" in query_keys:
+            options["word_count"] = abs(int(query["word_count"]))
+        if "phrase_count" in query_keys:
+            options["phrase_count"] = abs(int(query["phrase_count"]))
+        if "separator" in query_keys:
+            options["separator"] = query["separator"][0]
+        if "wordlist" in query_keys:
+            options["wordlist"] = query["wordlist"]
+
+        # validate
+        if options["word_count"] > MAX_WORDS:
+            options["word_count"] = MAX_WORDS
+        if options["word_count"] < 1:
+            raise Exception
+        if options["phrase_count"] > MAX_PHRASES:
+            options["phrase_count"] = MAX_PHRASES
+        if options["wordlist"] not in WORDLIST_FILES:
+            raise Exception
+        for k in query_keys:
+            if k not in DEFAULT_OPTS.keys():
+                raise Exception
+
+    except Exception:
+        return
+    return options
 
 
 @app.route("/", methods=["GET"])
 def api():
-    kwargs = {
-        "wordlist": "eff_short_wordlist_1.txt",
-        "word_count": 6,
-        "separator": " ",
-        "phrase_count": 1
-    }
-    wordlist_name = kwargs["wordlist"]
-    wordlist_files = get_wordlists(WORDLIST_DIR)
-    q = request.args
-    try:
-        if "word_count" in q:
-            value = abs(int(q["word_count"]))
-            if value > 30:
-                value = 30
-            kwargs["word_count"] = value
-        if "phrase_count" in q:
-            value = abs(int(q["phrase_count"]))
-            if value > 50:
-                value = 50
-            kwargs["phrase_count"] = value
-        if "separator" in q:
-            kwargs["separator"] = q["separator"][0]
-        if "wordlist" in q:
-            if q["wordlist"] not in wordlist_files:
-                raise Exception
-            wordlist_name = q["wordlist"]
-    except Exception:
+    options = parse_query(request.args)
+
+    if options is None:
         return make_response(
-            """Bad request. Check your query paramaters. Valid query paramaters
-            are: wordlist=(wordlist name), phrase_count=(integer),
-            word_count=(integer), separator=(character).""",
+            """Bad request. Check your query parameters. Valid query parameters
+                are: wordlist=(wordlist name), phrase_count=(integer),
+                word_count=(integer), separator=(character).""",
             HTTPStatus.BAD_REQUEST
         )
 
+    kwargs = options.copy()
     kwargs["wordlist"] = parse_wordlist_from_file(
-        WORDLIST_DIR + wordlist_name)
+        "business/wordlists/" + options["wordlist"])
     phrases = Phrases(**kwargs).as_dict()
-    phrases["wordlist"] = wordlist_name
 
     response = make_response({
-        "query": q,
-        "wordlists_available": wordlist_files,
+        "query": options,
+        "wordlists_available": WORDLIST_FILES,
         "phrases": phrases
     }, HTTPStatus.OK)
     response.headers["Access-Control-Allow-Origin"] = "*"
