66d474ad79fe6897d21ac60ae34fa01cd2b3c272
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-24 03:38:14 +0100

    Implement cerberus validation lib. Refactor flask api view

--- backend/api.py
+++ backend/api.py
@@ -1,58 +1,30 @@
 from http import HTTPStatus
 from flask import Flask, request, make_response
+from cerberus import Validator
 from business.diceware.classes.phrases import Phrases
-from business.diceware.parse_wordlist import from_file as parse_wordlist_from_file
+from business.diceware.parse_wordlist import from_file as parse_wordlist_file
 from helpers import get_wordlists
 
 app = Flask(__name__)
-WORDLIST_FILES = get_wordlists("business/wordlists/")
-DEFAULT_OPTS = {
-    "wordlist": "eff_short_wordlist_1.txt",
-    "word_count": 6,
-    "separator": " ",
-    "phrase_count": 1
+
+WORDLIST_DIR = "business/wordlists/"
+WORDLIST_FILES = get_wordlists(WORDLIST_DIR)
+QUERY_SCHEMA = {
+    "wordlist": {"type": "string", "default": "eff_short_wordlist_1.txt"},
+    "word_count": {
+        "type": "integer", "min": 1, "max": 30, "coerce": int, "default": 4},
+    "separator": {
+        "type": "string", "minlength": 1, "maxlength": 1, "default": " "},
+    "phrase_count": {
+        "type": "integer", "min": 0, "max": 50, "coerce": int, "default": 1},
 }
-MAX_WORDS = 30
-MAX_PHRASES = 50
-
-
-def parse_query(query):
-    options = DEFAULT_OPTS
-    query_keys = query.keys()
-    try:
-        # parse
-        if "word_count" in query_keys:
-            options["word_count"] = abs(int(query["word_count"]))
-        if "phrase_count" in query_keys:
-            options["phrase_count"] = abs(int(query["phrase_count"]))
-        if "separator" in query_keys:
-            options["separator"] = query["separator"][0]
-        if "wordlist" in query_keys:
-            options["wordlist"] = query["wordlist"]
-
-        # validate
-        if options["word_count"] > MAX_WORDS:
-            options["word_count"] = MAX_WORDS
-        if options["word_count"] < 1:
-            raise Exception
-        if options["phrase_count"] > MAX_PHRASES:
-            options["phrase_count"] = MAX_PHRASES
-        if options["wordlist"] not in WORDLIST_FILES:
-            raise Exception
-        for k in query_keys:
-            if k not in DEFAULT_OPTS.keys():
-                raise Exception
-
-    except Exception:
-        return
-    return options
 
 
 @app.route("/", methods=["GET"])
 def api():
-    options = parse_query(request.args)
-
-    if options is None:
+    query = dict(request.args)
+    validator = Validator()
+    if validator(query, QUERY_SCHEMA) is False:
         return make_response(
             """Bad request. Check your query parameters. Valid query parameters
                 are: wordlist=(wordlist name), phrase_count=(integer),
@@ -60,13 +32,14 @@ def api():
             HTTPStatus.BAD_REQUEST
         )
 
-    kwargs = options.copy()
-    kwargs["wordlist"] = parse_wordlist_from_file(
-        "business/wordlists/" + options["wordlist"])
+    # generate passphrases
+    kwargs = validator.document.copy()
+    kwargs["wordlist"] = parse_wordlist_file(
+        WORDLIST_DIR + kwargs["wordlist"])
     phrases = Phrases(**kwargs).as_dict()
 
     response = make_response({
-        "query": options,
+        "query": validator.document,
         "wordlists_available": WORDLIST_FILES,
         "phrases": phrases
     }, HTTPStatus.OK)
