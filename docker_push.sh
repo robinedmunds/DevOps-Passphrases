e7e3d078e5953c05bbf76e8ac427214ea4ea6210
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-29 15:29:01 +0100

    Travis: add docker hub push

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push roblobob/devops-passphrases-proxy
-docker push roblobob/devops-passphrases-api
-docker push roblobob/devops-passphrases-frontend
+
+docker push roblobob/passphrases-proxy:$TRAVIS_COMMIT
+docker push roblobob/passphrases-proxy:latest
+
+docker push roblobob/passphrases-api:$TRAVIS_COMMIT
+docker push roblobob/passphrases-api:latest
+
+docker push roblobob/passphrases-frontend:$TRAVIS_COMMIT
+docker push roblobob/passphrases-frontend:latest
